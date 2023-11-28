e1d119bcf692f45b24e7cf99cc1826eab797f48c
wagnersantosdev@gmail.com <114152919+WagSnt@users.noreply.github.com>
2023-11-28 00:21:00 -0300

    otimizando o código com for

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
-function tocaSomPom() {
-  document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio) {
+
+  document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
-//enquanto
-while (contador < listaDeTeclas.length) {
-  listaDeTeclas[contador].onclick = tocaSomPom;
+  const tecla = listaDeTeclas[contador];
+  const instrumento = tecla.classList[1]
+  const idAudio = `#som_${instrumento}`;//template string
 
-  contador = contador + 1;
-
-  console.log(contador);
+  tecla.onclick = function () {
+    tocaSom(idAudio)
+  };
 }
