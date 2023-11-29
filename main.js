1d41b9f6bbe42544c11380ff2a77453e0511aa1c
wagnersantosdev@gmail.com <114152919+WagSnt@users.noreply.github.com>
2023-11-28 21:09:28 -0300

    Projeto finalizado

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
-function tocaSom(idElementoAudio) {
+function tocaSom(seletorAudio) {
+  const elemento = document.querySelector(seletorAudio);
 
-  document.querySelector(idElementoAudio).play();
+  if (elemento && elemento.localName ==='audio') {
+    elemento.play();
+  }
+  else {
+    console.log('Elemento não encontrado ou seletor inválido')
+  }
+
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -15,4 +22,17 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
   tecla.onclick = function () {
     tocaSom(idAudio)
   };
+
+  tecla.onkeydown = function (evento) {
+
+    if (evento.code === 'Space' || evento.code === 'Enter') {
+      tecla.classList.add('ativa');
+    }
+    
+  }
+
+  tecla.onkeyup = function () {
+    tecla.classList.remove('ativa');
+  }
+
 }
